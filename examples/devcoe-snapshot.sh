ef3478c90eb2dc5d2b6bb9d27649238740ff24ab
Michael (Mike) Williams <34947926+mpw07458@users.noreply.github.com>
2018-09-16 15:55:56 -0400

    Create devcoe-snapshot.sh

---
+++ examples/devcoe-snapshot.sh
@@ -0,0 +1,6 @@
+kubectl get pvc azure-managed-disk
+az disk list --query '[].id | [?contains(@,`<pvc-id>`)]' -o tsv
+az snapshot create \
+    --resource-group MC_myResourceGroup_myAKSCluster_eastus \
+    --name pvcSnapshot \
+    --source /subscriptions/<guid>/resourceGroups/MC_myResourceGroup_myAKSCluster_eastus/providers/MicrosoftCompute/disks/kubernetes-dynamic-pvc-faf0f176-8b8d-11e8-923b-deb28c58d242
