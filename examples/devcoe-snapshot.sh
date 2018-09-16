441f0ef594ac994c6c7e0171c0240eed5a5bdf22
Michael (Mike) Williams <34947926+mpw07458@users.noreply.github.com>
2018-09-16 16:02:56 -0400

    Update devcoe-snapshot.sh

--- examples/devcoe-snapshot.sh
+++ examples/devcoe-snapshot.sh
@@ -4,3 +4,4 @@ az snapshot create \
     --resource-group MC_myResourceGroup_myAKSCluster_eastus \
     --name pvcSnapshot \
     --source /subscriptions/<guid>/resourceGroups/MC_myResourceGroup_myAKSCluster_eastus/providers/MicrosoftCompute/disks/kubernetes-dynamic-pvc-faf0f176-8b8d-11e8-923b-deb28c58d242
+az disk create --resource-group MC_myResourceGroup_myAKSCluster_eastus --name pvcRestored --source pvcSnapshot
